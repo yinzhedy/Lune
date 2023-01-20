458fd012eeb455a51cedbef96a4837347c145e80
yinzhedy <yinzhe.dy@gmail.com>
2023-01-20 18:26:06 -0500

    created basic layout for where header, piano, and footer will go.

---
+++ src/pages/homepage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Homepage = () => {
+    return (
+    <main id="grid-container">
+      <header class="header"> Lune
+      </header>
+
+      <div class="piano" id="keys">
+        <p>piano</p>
+      </div>
+
+
+      <footer class ="footer"> 
+        <p>for you</p>
+      </footer>
+
+    </main>
+      );
+    };
+    export default Homepage;
