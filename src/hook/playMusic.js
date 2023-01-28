7be42920e1c716eb5341841c8f56a992a45331be
yinzhedy <yinzhe.dy@gmail.com>
2023-01-27 21:51:03 -0500

    created playmusic hook, moved playmusic function into hook. iterate over imported audio files instead of finding correct audio element to play.

---
+++ src/hook/playMusic.js
@@ -0,0 +1,182 @@
+import React from 'react';
+import A0 from '../components/piano/audio-files/A-0.mp4';
+import A1 from '../components/piano/audio-files/A-1.mp4';
+import A2 from '../components/piano/audio-files/A-2.mp4';
+import A3 from '../components/piano/audio-files/A-3.mp4';
+import A4 from '../components/piano/audio-files/A-4.mp4';
+import A5 from '../components/piano/audio-files/A-5.mp4';
+import A6 from '../components/piano/audio-files/A-6.mp4';
+import A7 from '../components/piano/audio-files/A-7.mp4';
+import A0sharp from '../components/piano/audio-files/A-0#.mp4';
+import A1sharp from '../components/piano/audio-files/A-1#.mp4';
+import A2sharp from '../components/piano/audio-files/A-2#.mp4';
+import A3sharp from '../components/piano/audio-files/A-3#.mp4';
+import A4sharp from '../components/piano/audio-files/A-4#.mp4';
+import A5sharp from '../components/piano/audio-files/A-5#.mp4';
+import A6sharp from '../components/piano/audio-files/A-6#.mp4';
+import A7sharp from '../components/piano/audio-files/A-7#.mp4';
+
+
+// import B audio files
+import B0 from '../components/piano/audio-files/B-0.mp4';
+import B1 from '../components/piano/audio-files/B-1.mp4';
+import B2 from '../components/piano/audio-files/B-2.mp4';
+import B3 from '../components/piano/audio-files/B-3.mp4';
+import B4 from '../components/piano/audio-files/B-4.mp4';
+import B5 from '../components/piano/audio-files/B-5.mp4';
+import B6 from '../components/piano/audio-files/B-6.mp4';
+import B7 from '../components/piano/audio-files/B-7.mp4';
+
+
+// import C audio files
+import C1 from '../components/piano/audio-files/C-1.mp4';
+import C2 from '../components/piano/audio-files/C-2.mp4';
+import C3 from '../components/piano/audio-files/C-3.mp4';
+import C4 from '../components/piano/audio-files/C-4.mp4';
+import C5 from '../components/piano/audio-files/C-5.mp4';
+import C6 from '../components/piano/audio-files/C-6.mp4';
+import C7 from '../components/piano/audio-files/C-7.mp4';
+import C8 from '../components/piano/audio-files/C-8.mp4';
+import C1sharp from '../components/piano/audio-files/C-1#.mp4';
+import C2sharp from '../components/piano/audio-files/C-2#.mp4';
+import C3sharp from '../components/piano/audio-files/C-3#.mp4';
+import C4sharp from '../components/piano/audio-files/C-4#.mp4';
+import C5sharp from '../components/piano/audio-files/C-5#.mp4';
+import C6sharp from '../components/piano/audio-files/C-6#.mp4';
+import C7sharp from '../components/piano/audio-files/C-7#.mp4';
+
+
+// import D audio files
+import D1 from '../components/piano/audio-files/D-1.mp4';
+import D2 from '../components/piano/audio-files/D-2.mp4';
+import D3 from '../components/piano/audio-files/D-3.mp4';
+import D4 from '../components/piano/audio-files/D-4.mp4';
+import D5 from '../components/piano/audio-files/D-5.mp4';
+import D6 from '../components/piano/audio-files/D-6.mp4';
+import D7 from '../components/piano/audio-files/D-7.mp4';
+import D1sharp from '../components/piano/audio-files/D-1#.mp4';
+import D2sharp from '../components/piano/audio-files/D-2#.mp4';
+import D3sharp from '../components/piano/audio-files/D-3#.mp4';
+import D4sharp from '../components/piano/audio-files/D-4#.mp4';
+import D5sharp from '../components/piano/audio-files/D-5#.mp4';
+import D6sharp from '../components/piano/audio-files/D-6#.mp4';
+import D7sharp from '../components/piano/audio-files/D-7#.mp4';
+
+
+// import E audio files
+import E1 from '../components/piano/audio-files/E-1.mp4';
+import E2 from '../components/piano/audio-files/E-2.mp4';
+import E3 from '../components/piano/audio-files/E-3.mp4';
+import E4 from '../components/piano/audio-files/E-4.mp4';
+import E5 from '../components/piano/audio-files/E-5.mp4';
+import E6 from '../components/piano/audio-files/E-6.mp4';
+import E7 from '../components/piano/audio-files/E-7.mp4';
+
+
+// import F audio files
+import F1 from '../components/piano/audio-files/F-1.mp4';
+import F2 from '../components/piano/audio-files/F-2.mp4';
+import F3 from '../components/piano/audio-files/F-3.mp4';
+import F4 from '../components/piano/audio-files/F-4.mp4';
+import F5 from '../components/piano/audio-files/F-5.mp4';
+import F6 from '../components/piano/audio-files/F-6.mp4';
+import F7 from '../components/piano/audio-files/F-7.mp4';
+import F1sharp from '../components/piano/audio-files/F-1#.mp4';
+import F2sharp from '../components/piano/audio-files/F-2#.mp4';
+import F3sharp from '../components/piano/audio-files/F-3#.mp4';
+import F4sharp from '../components/piano/audio-files/F-4#.mp4';
+import F5sharp from '../components/piano/audio-files/F-5#.mp4';
+import F6sharp from '../components/piano/audio-files/F-6#.mp4';
+import F7sharp from '../components/piano/audio-files/F-7#.mp4';
+
+
+// import G audio files
+import G1 from '../components/piano/audio-files/G-1.mp4';
+import G2 from '../components/piano/audio-files/G-2.mp4';
+import G3 from '../components/piano/audio-files/G-3.mp4';
+import G4 from '../components/piano/audio-files/G-4.mp4';
+import G5 from '../components/piano/audio-files/G-5.mp4';
+import G6 from '../components/piano/audio-files/G-6.mp4';
+import G7 from '../components/piano/audio-files/G-7.mp4';
+import G1sharp from '../components/piano/audio-files/G-1#.mp4';
+import G2sharp from '../components/piano/audio-files/G-2#.mp4';
+import G3sharp from '../components/piano/audio-files/G-3#.mp4';
+import G4sharp from '../components/piano/audio-files/G-4#.mp4';
+import G5sharp from '../components/piano/audio-files/G-5#.mp4';
+import G6sharp from '../components/piano/audio-files/G-6#.mp4';
+import G7sharp from '../components/piano/audio-files/G-7#.mp4';
+
+let notes = [A0, A1, A2, A3, A4, A5, A6, A7,
+    B0, B1, B2, B3, B4, B5, B6, B7,
+    C1, C2, C3, C4, C5 ,C6, C7,
+    D1, D2, D3, D4, D5, D6, D7,
+    E1, E2, E3, E4, E5, E6, E7, 
+    F1, F2, F3, F4, F5, F6, F7,
+    G1, G2, G3, G4, G5, G6, G7
+    ]
+
+let notesSharp = [
+    A0sharp, A2sharp, A3sharp, A4sharp, A5sharp, A6sharp, A7sharp,
+    C1sharp, C2sharp, C3sharp, C4sharp, C5sharp, C6sharp, C7sharp,
+    D1sharp, D2sharp, D3sharp, D4sharp, D5sharp, D6sharp, D7sharp,
+    F1sharp, F2sharp, F3sharp, F4sharp, F5sharp, F6sharp, F7sharp,
+    G1sharp, G2sharp, G3sharp, G4sharp, G5sharp, G6sharp, G7sharp,
+]
+
+function playMusic(key) {
+    console.log(key)
+    // console.log(notes[0])
+
+    let clickedNote = key.dataset.note
+
+    console.log(clickedNote)
+
+    if (clickedNote.includes('#')) {
+        for (let i = 0; i < notesSharp.length; i++) {
+            if (notes[i].includes(clickedNote)) {
+                var audio = new Audio(notes[i]);
+                console.log(notes[i] + "this is notes i");
+                console.log(audio);
+                audio.play();
+            }
+          }
+    }
+
+    else {
+        for (let i = 0; i < notes.length; i++) {
+            if (notes[i].includes(clickedNote)) {
+                var audio = new Audio(notes[i]);
+                console.log(notes[i] + "this is notes i");
+                console.log(audio);
+                audio.play();
+            }
+          }
+
+    }
+
+    return;
+    // console.log('click')
+    // console.log(key);
+    // var note = function () {
+    //         this.path = '../components/piano/audio-files/';
+    //         this.noteName = key.dataset.note ;
+    //         this.notePath = this.path + this.noteName + '.mp4'
+    // };
+
+    // var playedNote = new note;
+    // console.log(playedNote);
+    // console.log(playedNote.path);
+    // console.log(playedNote.noteName);
+    // console.log(playedNote.notePath);
+
+    // var audio = new Audio(playedNote.notePath);
+    // console.log(audio);
+    // console.log(A0);
+    // audio.play()
+
+
+
+
+}
+
+export default playMusic;
